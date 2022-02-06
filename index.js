5bdfc321b85feb129cbba0cfd8fa5bf570561d6c
nlwkobe30 <nlwkobe30@gmail.com>
2022-02-07 01:30:43 +0800

    refactor some code

--- index.js
+++ index.js
@@ -1,24 +1,35 @@
 const express = require("express");
 const redis = require("redis");
 
-// constant
 const redisKey = "visiters";
 const port = 8080;
 
-// init redis client
-const redisClient = redis.createClient();
-(async () => {
+let redisClient;
+
+flow();
+
+async function flow() {
+  redisClient = await getRedisClient();
+
+  const app = express();
+
+  app.get("/", func);
+
+  app.listen(port, () => {
+    console.log(`listening on: http://localhost:${port}`);
+  });
+}
+
+async function getRedisClient() {
+  const redisClient = redis.createClient();
   await redisClient.connect();
+  console.log("redis connect");
+
+  // init some key
   await redisClient.set(redisKey, 0);
-})();
 
-const app = express();
-
-app.get("/", func);
-
-app.listen(port, () => {
-  console.log(`listening on: http://localhost:${port}`);
-});
+  return redisClient;
+}
 
 async function func(req, res) {
   const result = await redisClient.get(redisKey);
